d3cf1283d0f23412b8c4afb709a263336b477c91
Roman Romanik <r.romanik@dataclaritycorp.com>
2021-08-26 15:32:01 +0300

    create spring-boot app

---
+++ src/test/java/nik/roma/jokesapprr/JokesAppRrApplicationTests.java
@@ -0,0 +1,13 @@
+package nik.roma.jokesapprr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppRrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
